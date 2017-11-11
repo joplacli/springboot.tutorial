74c8766f18db9e01cec31c3e1aaec1164eb981ec
Josep Pla <pla@fzi.de>
2017-11-11 19:41:10 +0100

    Basic skeleton of a Springboot app

---
+++ src/main/java/de/ka/joplacli/springboottutorial/SpringBootTutorial.java
@@ -0,0 +1,13 @@
+package de.ka.joplacli.springboottutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  
+public class SpringBootTutorial {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorial.class, args);  
+	}
+
+}
